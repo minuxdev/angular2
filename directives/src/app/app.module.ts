391a2797c2566a599d20ec056ce3cb8910dd2434
minux dev <minux.midi@gmail.com>
2022-06-03 19:03:28 +0200

    Learned about built-in directives (structure and attribute directives)

---
+++ directives/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DirectivesComponent } from './directives/directives.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DirectivesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
